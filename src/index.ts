08c5db02dd0f288470266caf6684c38c888d6208
tihomirmagdic <tihomir.magdic@gmail.com>
2020-07-31 19:12:56 +0200

    fixed non-arrow fns

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 /**
- * 
+ *
  * Air quality measuring - REST API
- * 
+ *
  * version 1.0
  * created by Tihomir Magdic
  * created 2020-3-30
- * 
+ *
  * It uses PostgreSQL with pg-promise interface.
  * TODO: params, events, rules, log, security, config
- *   
+ *
  * Note: code structure is mainly based on Vitaly Tomilov (author of pg-promise)
  * https://github.com/vitaly-t/pg-promise-demo
- * 
+ *
  */
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-import express = require('express');
+import express = require("express");
 import * as bodyParser from "body-parser";
-import { db } from './db';
+import { db } from "./db";
 
 const app = express();
 app.use(bodyParser.json());
@@ -31,29 +31,31 @@ defineRoutes(app, config);
 
 const port = 5000;
 
-app.use(function(req, res, next) {
-  Promise.resolve().then(function () {
-    throw new Error('Path not found: ' + req.originalUrl)
-  }).catch(err => {
-		console.log(err);
-		res.status(400).end();
-	});
+app.use((req, res, next) => {
+  Promise.resolve()
+    .then(() => {
+      throw new Error("Path not found: " + req.originalUrl);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).end();
+    });
 });
 
 interface ResponseError extends Error {
   status?: number;
 }
 
-app.use(function(error: ResponseError, req: any, res: any, next: any) {
-	console.error("Bad request", error);
-  if (error instanceof SyntaxError && error.status === 400 && 'body' in error) {
-		console.error("JSON syntax error");
-	}
-	console.error(error.stack)
-	res.status(400).end();
-	//res.status(400).send('Bad request')
-})
+app.use((error: ResponseError, req: any, res: any, next: any) => {
+  console.error("Bad request", error);
+  if (error instanceof SyntaxError && error.status === 400 && "body" in error) {
+    console.error("JSON syntax error");
+  }
+  console.error(error.stack);
+  res.status(400).end();
+  //res.status(400).send('Bad request')
+});
 
 app.listen(port, () => {
-    console.log('\nReady for GET and other requests on http://localhost:' + port);
+  console.log("\nReady for GET and other requests on http://localhost:" + port);
 });
