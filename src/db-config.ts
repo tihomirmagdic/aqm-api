b1be38122060ea468ae953ca5145b86f47b87ad2
tihomirmagdic <tihomir.magdic@gmail.com>
2020-08-01 16:24:47 +0200

    changed dbConfig

--- src/db-config.ts
+++ src/db-config.ts
@@ -1,5 +1,6 @@
 export const dbConfig = {
   connectionString:
+    process.env.DATABASE_URL ||
     process.env.DATABASE_URL +
       "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" ||
     "postgres://nshsodqdoimnqo:9ba94ac902fa70628a04020137a5b91b9d9172b751b3b40e65daaf4864d4ca46@ec2-52-86-116-94.compute-1.amazonaws.com:5432/d9o5v8kne8u0su?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
