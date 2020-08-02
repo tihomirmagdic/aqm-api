ef6e0aa688ea9ddce6bef5c1f991c15c2418b2fd
tihomirmagdic <tihomir.magdic@gmail.com>
2020-08-02 11:28:42 +0200

    debug1

--- src/db-config.ts
+++ src/db-config.ts
@@ -4,9 +4,11 @@ export const dbConfig = {
   //  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" ||
   //"postgres://nshsodqdoimnqo:9ba94ac902fa70628a04020137a5b91b9d9172b751b3b40e65daaf4864d4ca46@ec2-52-86-116-94.compute-1.amazonaws.com:5432/d9o5v8kne8u0su?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
   //"postgres://nshsodqdoimnqo:9ba94ac902fa70628a04020137a5b91b9d9172b751b3b40e65daaf4864d4ca46@ec2-52-86-116-94.compute-1.amazonaws.com:5432/d9o5v8kne8u0su",
-  //host: "localhost",
-  //port: 5432,
+  // host: "localhost",
+  host: "ec2-52-86-116-94.compute-1.amazonaws.com",
+  port: 5432,
   //database: "postgres",
+  database: "d9o5v8kne8u0su",
   //user: "postgres",
   user: "nshsodqdoimnqo",
   password: "9ba94ac902fa70628a04020137a5b91b9d9172b751b3b40e65daaf4864d4ca46",
